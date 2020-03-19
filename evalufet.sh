19687c0dbae9aba12e090b9539e7b261ca3edc82
hldai <hldai@outlook.com>
2020-03-19 18:33:40 +0800

    evalufet.sh

---
+++ evalufet.sh
@@ -0,0 +1,1 @@
+python main.py et_model_eval -enhanced_mention -data_setup joint -add_crowd -multitask -mention_lstm -add_headword_emb -model_type et_model -mode test -reload_model_name et_model_best -eval_data crowd/dev_tree.json -load
